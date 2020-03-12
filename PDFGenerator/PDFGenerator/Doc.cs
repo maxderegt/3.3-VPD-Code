737bc208160545a754ff71ac76a12473568acc37
svber <a>
2020-03-12 15:50:09 +0100

    can create pdf, add lists of strings, add foto's

---
+++ PDFGenerator/PDFGenerator/Doc.cs
@@ -0,0 +1,50 @@
+﻿using iText.Kernel.Pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.IO.Image;
+
+namespace PDFGenerator
+{
+    class Doc
+    {
+        Document document;
+        public Doc(string destination)
+        {
+            FileInfo file = new FileInfo(destination);
+            file.Directory.Create();
+
+            PdfWriter writer = new PdfWriter(destination);
+            //Initialize PDF document
+            PdfDocument pdf = new PdfDocument(writer);
+            // Initialize document
+           document = new Document(pdf);
+        }
+
+        public void addStringList(List<string> array)
+        {
+            List list = new List().SetSymbolIndent(10).SetListSymbol("\u2022");
+            foreach (string s in array)
+            {
+                list.Add(s);
+            }
+
+            document.Add(list);
+        }
+
+        public void addPicture(string imgFile)
+        {
+            Image img = new Image(ImageDataFactory.Create(imgFile));
+            document.Add(img);
+        }
+
+        public void close()
+        {
+            document.Close();
+        }
+    }
+}
---
+++ PDFGenerator/PDFGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PDFGenerator
+{
+    class Program
+    {
+        //hij staat bij de .sln file
+        public const String DEST = "../../../../generatedPDF.pdf";
+
+        public static void Main(String[] args)
+        {
+            List<string> list = new List<string>() {"actie1", "actie2","actie3", "actie4", "actie5"};
+
+            //edit this path for the test image
+            string imgPath = "C:\\Users\\berni\\Downloads\\virtueel PD.jpg";
+
+            Doc doc = new Doc(DEST);
+            doc.addStringList(list);
+            doc.addPicture(imgPath);
+            doc.close();
+        }
+    }
+}
