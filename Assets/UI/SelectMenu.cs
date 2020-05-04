27496ee6f910749a4392cadbc2576147f26caf05
max <maxderegt@live.nl>
2020-05-04 14:09:43 +0200

    loads all scenes in automatically in scene selector

--- Assets/UI/SelectMenu.cs
+++ Assets/UI/SelectMenu.cs
@@ -6,19 +6,30 @@ using UnityEngine.UI;
 
 public class SelectMenu : MonoBehaviour
 {
-    List<Scene> allScenes;
+    public List<string> allScenes = new List<string>();
     public List<Button> sceneButtons;
+    public GameObject pfbutton;
+    public GameObject contentgrid;
 
     // Start is called before the first frame update
     void Start()
     {
         sceneButtons = new List<Button>();
-        allScenes = new List<Scene>();
-
-        for (int i = 0; i < SceneManager.sceneCount; i++)
+        int sceneCount = UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;
+        string[] scenes = new string[sceneCount];
+        for (int i = 0; i < sceneCount; i++)
         {
-            allScenes.Add(SceneManager.GetSceneAt(i));
-     
+            allScenes.Add(System.IO.Path.GetFileNameWithoutExtension(UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(i)));
+            GameObject button = Instantiate(pfbutton);
+            Text text = button.transform.GetChild(0).GetComponent(typeof(Text)) as Text;
+            text.text = allScenes[i];
+            Button b = button.GetComponent<Button>();
+            b.onClick.AddListener(delegate () {
+                Text name = b.transform.GetChild(0).GetComponent(typeof(Text)) as Text;
+                SceneManager.LoadScene(name.text); 
+            });
+            button.transform.parent = contentgrid.transform;
+            sceneButtons.Add(b);
         }
 
     }
