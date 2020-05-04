548d05a4eb1574bb075234d545de35d13ff749e0
sven <sven>
2020-05-04 13:41:21 +0200

    s

---
+++ Assets/UI/SelectMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectMenu : MonoBehaviour
+{
+    List<Scene> allScenes;
+    public List<Button> sceneButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneButtons = new List<Button>();
+        allScenes = new List<Scene>();
+
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            allScenes.Add(SceneManager.GetSceneAt(i));
+     
+        }
+
+    }
+
+    public void Select(string levelName)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
