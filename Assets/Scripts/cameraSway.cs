8d5c56bc6ee8ad3aab0e568a43f8149fbb5dca19
sven <sven>
2020-05-19 15:28:31 +0200

    cam sway

---
+++ Assets/Scripts/cameraSway.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraSway : MonoBehaviour
+{
+    public GameObject Camera;
+    float time;
+    float halfPI = Mathf.PI / 2;
+
+    // Update is called once per frame
+    Coroutine coroutine;
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        Camera.transform.Rotate(0, Mathf.Sin(time - halfPI) / 3, 0);
+
+    }
+
+}
